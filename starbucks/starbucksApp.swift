a7b07afab08ce6bd87ee726ff2d125f2159710df
epdsant <epdsant@gmail.com>
2024-04-03 11:35:59 +0100

    feat: First commit -> Project created

---
+++ starbucks/starbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  starbucksApp.swift
+//  starbucks
+//
+//  Created by EDSON SANTOS on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct starbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
